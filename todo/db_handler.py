64701d38db5a62e712c2cf6e019ddc5a381527cc
Stepan Ivanov <stepan.ivanov.dev@gmail.com>
2023-04-27 20:04:33 +0700

    Add a save method for the BaseModel
    At the same time, code refactoring was carried out.

--- todo/db_handler.py
+++ todo/db_handler.py
@@ -1,20 +1,36 @@
 import sqlite3
 from contextlib import closing
+from typing import Optional
 
 from settings import database
 from utils import form_filter_request
 
 
-def db_get(model: str, filter_fields: dict | None = None) -> list[dict]:
+def db_get(model: str, filter_fields: Optional[dict] = None) -> list[dict]:
     if filter_fields:
         filters = form_filter_request(filter_fields)
     else:
         filters = ''
 
+    query = f'SELECT * FROM {model}{filters};'
     with closing(sqlite3.connect(database)) as connection:
         connection.row_factory = sqlite3.Row
         cursor = connection.cursor()
-        cursor.execute(f'SELECT * FROM {model}{filters};')
+        cursor.execute(query)
         rows = cursor.fetchall()
-        values = [dict(row) for row in rows]
-        return values
+
+        records = [dict(row) for row in rows]
+        return records
+
+
+def db_add(model: str, data: dict) -> list[dict]:
+    keys = ', '.join(data.keys())
+    placeholders = ', '.join('?' * len(data))
+    query = f'INSERT INTO {model} ({keys}) VALUES ({placeholders})'
+    with closing(sqlite3.connect(database)) as connection:
+        cursor = connection.cursor()
+        cursor.execute(query, tuple(data.values()))
+        connection.commit()
+
+        record = {'id': cursor.lastrowid, **data}
+        return [record]
--- todo/models.py
+++ todo/models.py
@@ -1,10 +1,12 @@
-from db_handler import db_get
+from typing import Optional
+
+from db_handler import db_add, db_get
 
 
 class BaseModel():
     _fields = {}
 
-    def __validate_fields(self, fields):
+    def __validate_fields(self, fields: dict) -> bool:
         allowed_fields = self.__class__._fields
         allowed_fields['id'] = int()
         model = self.__class__.__name__
@@ -23,10 +25,19 @@ class BaseModel():
         if not kwargs:
             raise TypeError(f"{model}.filter() must accept "
                             f"at least 1 named argument")
-        if self.__validate_fields(fields=kwargs):
-            values = db_get(model, filter_fields=kwargs)
-            return values
+
+        self.__validate_fields(fields=kwargs)
+        records = db_get(model, filter_fields=kwargs)
+        return records
+
+    def save(self):
+        model = self.__class__.__name__
+        return db_add(model, data=self.__dict__)
 
 
 class Task(BaseModel):
     _fields = {'text': str()}
+
+    def __init__(self, text: Optional[str] = None) -> None:
+        if text:
+            self.text = text
