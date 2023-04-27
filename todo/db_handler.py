bcc667bffdeb1e8debd67f1b201e0237612ba8a5
Stepan Ivanov <stepan.ivanov.dev@gmail.com>
2023-04-27 18:51:09 +0700

    Change the type of data returned by db_get
    Now the function returns a list containing dictionaries.

--- todo/db_handler.py
+++ todo/db_handler.py
@@ -5,14 +5,16 @@ from settings import database
 from utils import form_filter_request
 
 
-def db_get(model, filter_fields=None):
+def db_get(model: str, filter_fields: dict | None = None) -> list[dict]:
     if filter_fields:
         filters = form_filter_request(filter_fields)
     else:
         filters = ''
 
     with closing(sqlite3.connect(database)) as connection:
+        connection.row_factory = sqlite3.Row
         cursor = connection.cursor()
         cursor.execute(f'SELECT * FROM {model}{filters};')
-        value = cursor.fetchall()
-        return value
+        rows = cursor.fetchall()
+        values = [dict(row) for row in rows]
+        return values
